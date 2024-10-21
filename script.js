4fcb7b3dfceebca018a621a745139d2caf6daeed
akryeziu <akryeziu@3cis.net>
2024-10-21 13:16:32 +0200

    Reset All

--- script.js
+++ script.js
@@ -33,18 +33,28 @@ function resetSelected() {
   });
 }
 
+// Reset Score & playerChoice/computerChoice
+function resterAll() {
+  playerScoreNumber = 0;
+  computerScoreNumber = 0;
+  playerScoreEl.textContent = playerScoreNumber;
+  computerScoreEl.textContent = computerScoreNumber;
+  playerChoiceEl.textContent = "";
+  computerChoiceEl.textContent = "";
+  resultText.textContent = "";
+  resetSelected();
+}
+
 let playerScoreNumber = 0;
 let computerScoreNumber = 0;
 let computerChoice = "";
 
 // Check result, increase scores, update resultText
 function updateScore(playerChoice) {
-  console.log(playerChoice, computerChoice);
   if (playerChoice === computerChoice) {
     resultText.textContent = "It's a tie";
   } else {
     const choice = choices[playerChoice];
-    console.log(choice.defeats.indexOf(computerChoice));
     if (choice.defeats.indexOf(computerChoice) > -1) {
       resultText.textContent = "You Won!";
       playerScoreNumber++;
@@ -138,3 +148,6 @@ function select(playerChoice) {
       break;
   }
 }
+
+// On startup, set initial values
+resterAll();
